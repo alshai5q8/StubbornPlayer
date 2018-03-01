d6e5995684e6850c8074f2871d3e189b47105862
salshaikh <salshaikh@mail.csuchico.edu>
2018-02-28 23:07:08 -0800

    Time Won!

--- main.cpp
+++ main.cpp
@@ -5,7 +5,52 @@ using namespace std;
 
 int main()
 {
-  SecretDoor game;
-    
-  return 0;
+        SecretDoor game;
+
+
+
+        int PlayedGames  = 0;
+
+        int WonGames = 0;
+
+        char input;
+
+
+
+
+        cout<<"How many games you want to play?"<<endl;
+
+        cin>>PlayedGames;
+
+
+
+                for(int i = 0; i < PlayedGames; i++) 
+                {
+
+
+                        game.newGame();
+
+
+                        game.guessDoorC();
+                        game.guessDoorC();
+
+
+
+                       if ( game.isWinner() == true)
+
+
+
+
+                        {
+
+                                WonGames++;
+
+                        }
+                }
+
+        cout<<"You won this many "<<WonGames<<" times!"<<endl;
+
+
+
+        return 0;
 }
